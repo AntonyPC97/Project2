0b7443a2d17ed1b90e626ceeac830780129589c3
Antony Palacios <apalaciosc@pucp.edu.pe>
2020-08-25 13:31:41 -0500

    Verificar creacion de Auction

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Auction(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    date_listed = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='uploads/')
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} by {self.user}"
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,9 +3,16 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from .models import User,Auction,Category
 
-from .models import User
-
+class NewAuctionForm(forms.Form):
+    categories = Category.objects.all()
+    name = forms.CharField(max_length=50)
+    description = forms.CharField(widget=forms.Textarea)
+    image=forms.ImageField()
+    price = forms.FloatField(label='Initial Price')
+    category = forms.ModelChoiceField(queryset=categories)
 
 def index(request):
     return render(request, "auctions/index.html")
@@ -61,3 +68,21 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def newAuction(request):
+    if(request.method=='POST'):
+        form = NewAuctionForm(request.POST)
+        if(form.is_valid()):
+            name=form.cleaned_data['name']
+            description=form.cleaned_data['description']
+            price = float(form.cleaned_data['price'])
+            category = form.cleaned_data['category']
+            image = form.cleaned_data['image']
+            auction = Auction(name=name,description=description,price=price,
+                              category=Category.objects.get(pk=category),image=image)
+            auction.save()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request,'auctions/newAuction.html',{'form':form})
+    else:
+        return render(request,'auctions/newAuction.html',{'form':NewAuctionForm()})
