6a7c7c757f57698fb155e28b95bfcee6fb711302
Antony Palacios <apalaciosc@pucp.edu.pe>
2020-08-26 18:45:14 -0500

    Sirve watchlist, falta remover watchlist y subasta

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Auction(models.Model):
+class Listing(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.FloatField()
@@ -23,3 +23,27 @@ class Auction(models.Model):
 
     def __str__(self):
         return f"{self.name} by {self.user}"
+
+class Bid(models.Model):
+    bid = models.FloatField()
+    listing = models.ForeignKey(Listing,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Bid of {round(self.bid,2)} place by {self.user.username} on item {self.listing.id}'
+
+class Comment(models.Model):
+    comment = models.TextField()
+    listing = models.ForeignKey(Listing,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date_comment = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} commented on item {self.listing.id}'
+
+class Watchlist(models.Model):
+    listing = models.ForeignKey(Listing,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} watchlist item {self.listing.id}'
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,20 +1,26 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
 from django import forms
-from .models import User,Auction,Category
+from .models import User,Listing,Category,Bid,Comment,Watchlist
 from django.views.generic import DetailView
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.forms import ModelForm
+
+class ListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = '__all__'
 
 def index(request):
-    #Get all auctions from the logged in user
-    if(request.user.id is not None):
-        auctions = Auction.objects.filter(user = request.user).all()
-        return render(request, "auctions/index.html",{'auctions':auctions})
-    else:
-        return render(request, "auctions/index.html")
-
+    #Get all listing 
+    listings = Listing.objects.all()
+    return render(request, "auctions/index.html",{
+        'auctions':listings
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -67,7 +73,7 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
-class NewAuctionForm(forms.Form):
+class NewListingForm(forms.Form):
     categories = Category.objects.all()
     name = forms.CharField(max_length=50)
     description = forms.CharField(widget=forms.Textarea)
@@ -75,20 +81,56 @@ class NewAuctionForm(forms.Form):
     price = forms.FloatField(label='Initial Price')
     category = forms.ModelChoiceField(queryset=categories)
 
-def newAuction(request):
+
+def newListing(request):
     if(request.method=='POST'):
-        form = NewAuctionForm(request.POST,request.FILES)
+        form = NewListingForm(request.POST,request.FILES)
         if(form.is_valid()):
             name=form.cleaned_data['name']
             description=form.cleaned_data['description']
             price = float(form.cleaned_data['price'])
             category = Category.objects.get(pk=form.cleaned_data['category'].id)
             image = request.FILES['image']
-            auction = Auction.objects.create(name=name,description=description,price=price,
+            listing = Listing.objects.create(name=name,description=description,price=price,
                               category=category,image=image,user = request.user,active=True)
-            auction.save()
+            listing.save()
             return HttpResponseRedirect(reverse('index'))
         else:
-            return render(request,'auctions/newAuction.html',{'form':form})
+            return render(request,'auctions/newListing.html',{'form':form})
     else:
-        return render(request,'auctions/newAuction.html',{'form':NewAuctionForm()})
+        return render(request,'auctions/newListing.html',{'form':NewListingForm()})
+
+def itemDetail(request,item_name):
+    if(request.method=='POST'):
+        listing = Listing.objects.get(name=item_name)
+        #check if it's already in his watchlist
+        try:
+            watchlist = Watchlist.objects.get(user=request.user,listing=listing)
+        except ObjectDoesNotExist:
+            watchlist = Watchlist.objects.create(user=request.user,listing=listing)
+            watchlist.save()
+    try:
+        item = Listing.objects.get(name=item_name)
+        return render(request,'auctions/itemDetail.html',{'item':item})
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('index'))
+    
+def listCategories(request):
+    categories = Category.objects.all()
+    return render(request,'auctions/listCategories.html',{'categories':categories})
+
+def itemByCategory(request,category):
+    category = Category.objects.get(name=category)
+    listings = category.listing_set.all()
+    return render(request,'auctions/index.html',{'auctions':listings})
+
+@login_required
+def listWatchlist(request):
+    try:
+        user = request.user
+        listings = [watchlist.listing for watchlist in user.watchlist_set.all()]
+        return render(request,'auctions/index.html',{
+            'auctions':listings,
+            'text':f'Watchlist for {user.username}'})
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('index'))
